974d0145befda768a1876d9a0e7e64835c31811b
Peter Pham <peterpham86@gmail.com>
2021-01-03 12:11:36 -0800

    update build script to auto detect VS version

--- build.bat
+++ build.bat
@@ -1,6 +1,15 @@
-REM delete output folder 
+@SETLOCAL
+@REM delete output folder 
 if exist build rd /s /q build
 mkdir build
 cd build
-cmake -G "Visual Studio 15 2017 Win64" ..
+@REM https://github.com/microsoft/vswhere/wiki/Find-VC
+set VSWHERE="%ProgramFiles(x86)%\Microsoft Visual Studio\Installer\vswhere.exe"
+for /f "usebackq tokens=*" %%i in (`%VSWHERE% -latest -products * -requires Microsoft.VisualStudio.Component.VC.Tools.x86.x64 -property installationPath`) do (
+  set InstallDir=%%i
+)
+CALL "%InstallDir%\Common7\Tools\vsdevcmd.bat" -arch=x64 -host_arch=x64
+@IF ERRORLEVEL 1 EXIT /B 1
+cmake -G "NMake Makefiles" ..
+@IF ERRORLEVEL 1 EXIT /B 1
 cmake --build . --config Release
